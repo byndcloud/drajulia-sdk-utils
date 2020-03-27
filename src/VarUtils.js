aaf96b656d440e6d7bd9e880aac1181462838d13
Arthur Lapprand <lapprand@gmail.com>
2020-03-27 19:15:46 -0300

    refactor: remove try catch

--- src/VarUtils.js
+++ src/VarUtils.js
@@ -1,86 +1,79 @@
 export default class VarUtils {
     /**
-     * 
+     *
      * @param {String} completeVarName The complete name for this variable which is contained in the docx file. Ex.: {{varName}}
      */
     getVarName(completeVarName) {
-        try {
-            if (completeVarName.startsWith('_gen')) {
-                let { groups: { variavel } } = /_gen(?<variavel>.+)\[\[(?<fem>.+)\]\].*\[\[(?<masc>.+)\]\].*/gm.exec(completeVarName)
-                variavel = variavel.trim()
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_num')) {
-                let variavel
-                if (completeVarName.endsWith('Extenso')) {
-                    variavel = completeVarName.substring(4, completeVarName.length - 7)
-                } else {
-                    variavel = completeVarName.substring(4, completeVarName.length)
-                }
-                variavel = variavel.trim()
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_real')) {
-                let variavel
-                if (completeVarName.endsWith('Extenso')) {
-                    variavel = completeVarName.substring(5, completeVarName.length - 7)
-                } else {
-                    variavel = completeVarName.substring(5, completeVarName.length)
-                }
-                variavel = variavel.trim()
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_dat')) {
-                let variavel
-                if (completeVarName.endsWith('Extenso')) {
-                    variavel = completeVarName.substring(4, completeVarName.length - 7)
-                } else {
-                    variavel = completeVarName.substring(4, completeVarName.length)
-                }
-                variavel = variavel.trim()
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_sel')) {
-                let { groups: { variavel, valor } } = /_sel(?<variavel>.+)_(?<valor>.+)_.+/gm.exec(completeVarName)
-                variavel = variavel.trim()
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('IMAGE') && completeVarName.endsWith('()')) {
-                const { groups: { variavel } } = /IMAGE\s+(?<variavel>.+)[(][)]/gm.exec(completeVarName)
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_cpf')) {
-                let variavel = completeVarName.replace(/\s+/g, '')
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_tel')) {
-                let variavel = completeVarName.replace(/\s+/g, '')
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('_cep')) {
-                let variavel = completeVarName.replace(/\s+/g, '')
-                return variavel
-            }
-
-            else if (completeVarName.startsWith('@tabela')) {
-                let variavel = completeVarName.substring(7, completeVarName.length).trim()
-                return variavel
-            } else if (completeVarName.startsWith('@topic')) {
-                let variavel = completeVarName.substring(6, completeVarName.length).trim()
-                return variavel
+        if (completeVarName.startsWith('_gen')) {
+            let {
+                groups: { variavel },
+            } = /_gen(?<variavel>.+)\[\[(?<fem>.+)\]\].*\[\[(?<masc>.+)\]\].*/gm.exec(
+                completeVarName
+            );
+            variavel = variavel.trim();
+            return variavel;
+        } else if (completeVarName.startsWith('_num')) {
+            let variavel;
+            if (completeVarName.endsWith('Extenso')) {
+                variavel = completeVarName.substring(4, completeVarName.length - 7);
             } else {
-                return completeVarName
+                variavel = completeVarName.substring(4, completeVarName.length);
             }
-        } catch (error) {
-            console.error(error)
+            variavel = variavel.trim();
+            return variavel;
+        } else if (completeVarName.startsWith('_real')) {
+            let variavel;
+            if (completeVarName.endsWith('Extenso')) {
+                variavel = completeVarName.substring(5, completeVarName.length - 7);
+            } else {
+                variavel = completeVarName.substring(5, completeVarName.length);
+            }
+            variavel = variavel.trim();
+            return variavel;
+        } else if (completeVarName.startsWith('_dat')) {
+            let variavel;
+            if (completeVarName.endsWith('Extenso')) {
+                variavel = completeVarName.substring(4, completeVarName.length - 7);
+            } else {
+                variavel = completeVarName.substring(4, completeVarName.length);
+            }
+            variavel = variavel.trim();
+            return variavel;
+        } else if (completeVarName.startsWith('_sel')) {
+            let {
+                groups: { variavel, valor },
+            } = /_sel(?<variavel>.+)_(?<valor>.+)_.+/gm.exec(completeVarName);
+            variavel = variavel.trim();
+            return variavel;
+        } else if (
+            completeVarName.startsWith('IMAGE') &&
+            completeVarName.endsWith('()')
+        ) {
+            const {
+                groups: { variavel },
+            } = /IMAGE\s+(?<variavel>.+)[(][)]/gm.exec(completeVarName);
+            return variavel;
+        } else if (completeVarName.startsWith('_cpf')) {
+            let variavel = completeVarName.replace(/\s+/g, '');
+            return variavel;
+        } else if (completeVarName.startsWith('_tel')) {
+            let variavel = completeVarName.replace(/\s+/g, '');
+            return variavel;
+        } else if (completeVarName.startsWith('_cep')) {
+            let variavel = completeVarName.replace(/\s+/g, '');
+            return variavel;
+        } else if (completeVarName.startsWith('@tabela')) {
+            let variavel = completeVarName
+                .substring(7, completeVarName.length)
+                .trim();
+            return variavel;
+        } else if (completeVarName.startsWith('@topic')) {
+            let variavel = completeVarName
+                .substring(6, completeVarName.length)
+                .trim();
+            return variavel;
+        } else {
+            return completeVarName;
         }
     }
 }
